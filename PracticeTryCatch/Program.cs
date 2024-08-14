592e36591d85b8bcfdfe875e7bebfd9cbe120859
Kaan Kaya <kaankaya.0707@gmail.com>
2024-08-14 03:13:48 +0300

    Add project files.

---
+++ PracticeTryCatch/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //
+        Tekrar:
+        try
+        {
+            Console.WriteLine("Bir Sayı Giriniz");
+            double input = Convert.ToDouble(Console.ReadLine());
+            double kare = input * input;
+            Console.WriteLine($"Girdiğiniz Sayının Karesi {kare}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Hatalı Bir Giriş Yaptınız");
+            goto Tekrar;
+        }
+        finally
+        {
+            Console.WriteLine("İşlem Tamamlandı");
+        }
+    }
+}
